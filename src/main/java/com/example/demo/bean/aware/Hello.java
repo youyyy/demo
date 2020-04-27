e8570bae54d703c3a2cd82256a491757b06151cf
youyi <zndx.2015.you@gmail.com>
2020-04-27 17:41:52 +0800

    aware使用

---
+++ src/main/java/com/example/demo/bean/aware/Hello.java
@@ -0,0 +1,28 @@
+package com.example.demo.bean.aware;
+/*
+          .--,       .--,
+         ( (  \.---./  ) )
+          '.__/o   o\__.'
+             {=  ^  =}
+              >  -  <
+             /       \
+            //       \\
+           //|   .   |\\
+           "'\       /'"_.-~^`'-.
+              \  _  /--'         `
+            ___)( )(___
+           (((__) (__)))
+   高山仰止,景行行止.虽不能至,心向往之。
+*/
+
+/**
+ * @program: demo
+ * @description: description
+ * @author: youyi
+ * @create: 2020-04-27 17:36
+ **/
+public class Hello {
+    public void say(){
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/com/example/demo/bean/aware/TestAware.java
@@ -0,0 +1,50 @@
+package com.example.demo.bean.aware;
+/*
+          .--,       .--,
+         ( (  \.---./  ) )
+          '.__/o   o\__.'
+             {=  ^  =}
+              >  -  <
+             /       \
+            //       \\
+           //|   .   |\\
+           "'\       /'"_.-~^`'-.
+              \  _  /--'         `
+            ___)( )(___
+           (((__) (__)))
+   高山仰止,景行行止.虽不能至,心向往之。
+*/
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @program: demo
+ * @description: aware用法
+ * @author: youyi
+ * @create: 2020-04-27 17:28
+ **/
+public class TestAware implements BeanFactoryAware {
+    private BeanFactory beanFactory;
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    public void testAware() {
+        Hello hello = (Hello) beanFactory.getBean("hello");
+        hello.say();
+    }
+
+
+    public static void main(String[] args) {
+        // 需要写一下
+        ApplicationContext ct = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestAware testAware = (TestAware) ct.getBean("test");
+        testAware.testAware();
+    }
+}
