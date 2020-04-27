44430e66672a92d68f711a9b32cb6663b12c37ad
youyi <zndx.2015.you@gmail.com>
2020-04-27 18:00:32 +0800

    initMethods

--- src/main/java/com/example/demo/bean/DI/Word.java
+++ src/main/java/com/example/demo/bean/DI/Word.java
@@ -12,6 +12,10 @@ public class Word {
     public void setMessage(String message){
         this.message  = message;
     }
+    public void  initTest(){
+        System.out.println("init: " + message);
+
+    }
     public void getMessage(){
         System.out.println("Your word : " + message);
     }
