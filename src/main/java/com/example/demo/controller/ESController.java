594fab793b23bf0bc01ea90ef420aeb0f0f8a00c
youyi <zndx.2015.you@gmail.com>
2020-05-11 14:36:46 +0800

    springboot集成es

--- src/main/java/com/example/demo/controller/ESController.java
+++ src/main/java/com/example/demo/controller/ESController.java
@@ -52,13 +52,13 @@ public class ESController {
 
         Article article = new Article();
         article.setId(1L);
-        article.setTitle("springboot integreate elasticsearch");
-        article.setAbstracts("springboot integreate elasticsearch is very easy");
+        article.setTitle("游移 integreate elasticsearch");
+        article.setAbstracts("游移 integreate elasticsearch is very easy");
         article.setTutorial(tutorial);
         article.setAuthor(author);
-        article.setContent("elasticsearch based on lucene,"
+        article.setContent("elasticsearch based 游移 lucene,"
                 + "spring-data-elastichsearch based on elaticsearch"
-                + ",this tutorial tell you how to integrete springboot with spring-data-elasticsearch");
+                + ",this tutorial 游移 you how to integrete 游移 with spring-data-elasticsearch");
         article.setPostTime(System.currentTimeMillis()/1000);
         article.setClickCount(1L);
 
@@ -66,7 +66,7 @@ public class ESController {
     }
     @GetMapping("/query")
     public Results testSearch() {
-        String queryString = "springboot";//搜索关键字
+        String queryString = "游移";//搜索关键字
         QueryStringQueryBuilder builder = new QueryStringQueryBuilder(queryString);
         Iterable<Article> searchResult = articleSearchRepository.search(builder);
         Iterator<Article> iterator = searchResult.iterator();
