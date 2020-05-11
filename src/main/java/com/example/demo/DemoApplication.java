85292ddb1093875c7e2b1d0a23da9d27049b6b6a
youyi <zndx.2015.you@gmail.com>
2020-05-11 14:59:38 +0800

    注释掉datasource

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication(scanBasePackages = "com.example.demo")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class},scanBasePackages = "com.example.demo")
 /*这里给注册上了word*/
 @ImportResource(locations = "classpath:xml/Beans.xml")
 public class DemoApplication {
