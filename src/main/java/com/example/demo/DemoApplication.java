76b3d1d7305745d76fbae90014ab0dd852259a05
youyi <zndx.2015.you@gmail.com>
2020-05-11 14:56:53 +0800

    springboot扫描包指定

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.demo")
 /*这里给注册上了word*/
 @ImportResource(locations = "classpath:xml/Beans.xml")
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 
 	}
 }
