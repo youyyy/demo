b3921f1423907ec056659f31065287d49759c150
youyi <zndx.2015.you@gmail.com>
2020-06-04 17:25:50 +0800

    自定义上下文

--- src/main/java/com/example/demo/bean/context/MyContext.java
+++ src/main/java/com/example/demo/bean/context/MyContext.java
@@ -21,6 +21,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * @program: demo
  * @description: 学习ApplicationContext
+ *
+ * 自定义上下文
+ *
  * @author: youyi
  * @create: 2020-04-28 15:53
  **/
@@ -35,6 +38,7 @@ public class MyContext extends ClassPathXmlApplicationContext {
     protected void initPropertySources() {
         // For subclasses: do nothing by default.
         // 必须要有的一些环境变量
+        // 如果没有就报错
         getEnvironment().setRequiredProperties("VAR");
     }
 
